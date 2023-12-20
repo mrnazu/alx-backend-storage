551835c364b8504aa98923235f0cf5a968588ee2
mrnazu <samuelamsalu1998@gmail.com>
2023-12-20 12:08:16 +0300

    3. Storing lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,7 @@ class Cache:
         self._redis = redis.Redis(host=host, port=port, db=db)
         self._redis.flushdb()
 
-    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         if isinstance(data, (int, float)):
@@ -28,21 +28,42 @@ class Cache:
     def get_int(self, key: str) -> Union[int, None]:
         return self.get(key, fn=int)
 
-def count_calls(method: Callable) -> Callable:
+def call_history(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        key = f"{method.__qualname__}_calls"
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        # Append input parameters to the input list
+        self._redis.rpush(input_key, str(args))
+
+        # Execute the wrapped function to retrieve the output
+        result = method(self, *args, **kwargs)
+
+        # Append the output to the output list
+        self._redis.rpush(output_key, str(result))
+
+        return result
     return wrapper
 
 # Example usage and test
 cache = Cache()
 
 # Calling store multiple times
-for _ in range(5):
+for _ in range(3):
     cache.store("Hello, Redis!")
 
-# Retrieving the count of store calls
-store_calls_count = cache._redis.get("Cache.store_calls")
-print(f"Number of store calls: {int(store_calls_count)}")
+# Retrieving the input and output history
+input_history_key = "Cache.store:inputs"
+output_history_key = "Cache.store:outputs"
+
+input_history = cache._redis.lrange(input_history_key, 0, -1)
+output_history = cache._redis.lrange(output_history_key, 0, -1)
+
+print("Input History:")
+for entry in input_history:
+    print(entry.decode('utf-8'))
+
+print("\nOutput History:")
+for entry in output_history:
+    print(entry.decode('utf-8'))
