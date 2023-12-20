a6bfaafdde1779c3cb3f4f94e1c7feb3dacca03d
mrnazu <samuelamsalu1998@gmail.com>
2023-12-20 11:16:12 +0300

    1. Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,29 +1,47 @@
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     def __init__(self, host='localhost', port=6379, db=0):
-        # Initialize the Redis client and flush the database
         self._redis = redis.Redis(host=host, port=port, db=db)
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        # Generate a random key using uuid
         key = str(uuid.uuid4())
-
-        # Store the data in Redis with the generated key
         if isinstance(data, (int, float)):
-            # Convert int or float to string before storing
             data = str(data)
-
         self._redis.set(key, data)
-
-        # Return the generated key
         return key
 
-# Example usage:
-cache_instance = Cache()
-key = cache_instance.store("Hello, Redis!")
-print(f"Data stored with key: {key}")
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        data = self._redis.get(key)
+        if data is not None:
+            return fn(data) if fn else data
+        return None
 
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda d: d.decode('utf-8') if d else None)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=int)
+
+# Example usage and test cases
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+
+# Additional test cases using get_str and get_int
+str_key = cache.store("Hello, Redis!")
+int_key = cache.store(42)
+
+assert cache.get_str(str_key) == "Hello, Redis!"
+assert cache.get_int(int_key) == 42
