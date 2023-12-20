f9f8c24c2b684de163374b3049a86635e8136b09
mrnazu <samuelamsalu1998@gmail.com>
2023-12-20 12:23:10 +0300

    5. Implementing an expiring web cache and tracker: DONE

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,53 +7,30 @@ import requests
 from functools import wraps
 from typing import Callable
 
+
 def track_get_page(fn: Callable) -> Callable:
     """ Decorator for get_page
     """
     @wraps(fn)
     def wrapper(url: str) -> str:
         """ Wrapper that:
-            - checks whether a URL's data is cached
+            - check whether a url's data is cached
             - tracks how many times get_page is called
         """
-        # Create a Redis client
         client = redis.Redis()
-
-        # Increment the access count for the URL
         client.incr(f'count:{url}')
-
-        # Check if cached data exists for the URL
         cached_page = client.get(f'{url}')
         if cached_page:
-            # Return cached data if available
             return cached_page.decode('utf-8')
-
-        # Call the original function to fetch data
         response = fn(url)
-
-        # Cache the response with a 10-second expiration
-        client.setex(f'{url}', 10, response)
-
-        # Return the fetched data
+        client.set(f'{url}', response, 10)
         return response
-
     return wrapper
+
 
 @track_get_page
 def get_page(url: str) -> str:
-    """ Makes an HTTP request to a given endpoint
+    """ Makes a http request to a given endpoint
     """
-    # Make an HTTP request using the requests module
     response = requests.get(url)
-
-    # Return the text content of the response
     return response.text
-
-# Example usage
-if __name__ == "__main__":
-    # Example of calling the get_page function with caching and slow response simulation
-    url_to_fetch = "http://slowwly.robertomurray.co.uk"
-    result = get_page(url_to_fetch)
-    
-    # Print the result
-    print(f"Content fetched from {url_to_fetch}:\n{result}")
