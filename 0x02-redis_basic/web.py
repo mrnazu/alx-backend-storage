2c11711f52be3d7e46c4e3ba9a59920079030c4c
mrnazu <samuelamsalu1998@gmail.com>
2023-12-20 12:21:40 +0300

    5. Implementing an expiring web cache and tracker: DONE

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,82 +1,59 @@
-import requests
+#!/usr/bin/env python3
+"""
+Caching request module
+"""
 import redis
+import requests
 from functools import wraps
 from typing import Callable
 
-# Create a Redis connection
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
-
-def track_access_count(url: str) -> None:
-    count_key = f"count:{url}"
-    redis_client.incr(count_key)
-
-def cache_with_expiry(url: str, content: str) -> None:
-    cache_key = f"cache:{url}"
-    redis_client.setex(cache_key, 10, content)
-
-def get_page(url: str) -> str:
-    # Check if the content is cached
-    cache_key = f"cache:{url}"
-    cached_content = redis_client.get(cache_key)
-
-    if cached_content:
-        track_access_count(url)
-        return cached_content.decode('utf-8')
-
-    # Fetch content using requests
-    response = requests.get(url)
-    content = response.text
-
-    # Cache the content with an expiry time of 10 seconds
-    cache_with_expiry(url, content)
-    track_access_count(url)
-
-    return content
-
-# Decorator to add caching and tracking to functions
-def web_cache(func: Callable) -> Callable:
-    @wraps(func)
+def track_get_page(fn: Callable) -> Callable:
+    """ Decorator for get_page
+    """
+    @wraps(fn)
     def wrapper(url: str) -> str:
-        # Check if the content is cached
-        cache_key = f"cache:{url}"
-        cached_content = redis_client.get(cache_key)
+        """ Wrapper that:
+            - checks whether a URL's data is cached
+            - tracks how many times get_page is called
+        """
+        # Create a Redis client
+        client = redis.Redis()
 
-        if cached_content:
-            track_access_count(url)
-            return cached_content.decode('utf-8')
+        # Increment the access count for the URL
+        client.incr(f'count:{url}')
 
-        # Execute the original function
-        content = func(url)
+        # Check if cached data exists for the URL
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            # Return cached data if available
+            return cached_page.decode('utf-8')
 
-        # Cache the content with an expiry time of 10 seconds
-        cache_with_expiry(url, content)
-        track_access_count(url)
+        # Call the original function to fetch data
+        response = fn(url)
 
-        return content
+        # Cache the response with a 10-second expiration
+        client.setex(f'{url}', 10, response)
+
+        # Return the fetched data
+        return response
 
     return wrapper
 
-# Usage of the decorator
-@web_cache
-def get_page_with_cache(url: str) -> str:
-    # Fetch content using requests
+@track_get_page
+def get_page(url: str) -> str:
+    """ Makes an HTTP request to a given endpoint
+    """
+    # Make an HTTP request using the requests module
     response = requests.get(url)
-    content = response.text
-    return content
+
+    # Return the text content of the response
+    return response.text
 
 # Example usage
 if __name__ == "__main__":
-    # Example usage of the get_page function
-    url_to_fetch = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://www.example.com"
+    # Example of calling the get_page function with caching
+    url_to_fetch = "https://www.example.com"
     result = get_page(url_to_fetch)
+    
+    # Print the result
     print(f"Content fetched from {url_to_fetch}:\n{result}")
-
-    # Example usage of the decorated get_page_with_cache function
-    url_to_fetch_cached = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://www.example.com"
-    result_cached = get_page_with_cache(url_to_fetch_cached)
-    print(f"\nContent fetched from {url_to_fetch_cached} using caching:\n{result_cached}")
-
-    # Example tracking the access count
-    access_count_key = f"count:{url_to_fetch}"
-    count = redis_client.get(access_count_key)
-    print(f"\nAccess count for {url_to_fetch}: {int(count)}")
